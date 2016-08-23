7cc2ec6066816961e91d562424cf7ee49a6f2eb3
201601180154 <zmhust88@gmail.com>
2016-08-23 12:03:35 +0800

    modify protobuf io

--- push-io/src/main/java/push/SecurePushServerHandler.java
+++ push-io/src/main/java/push/SecurePushServerHandler.java
@@ -3,12 +3,9 @@ package push;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.channel.group.ChannelGroup;
-import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.handler.ssl.SslHandler;
 import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.GenericFutureListener;
-import io.netty.util.concurrent.GlobalEventExecutor;
 import push.message.Entity;
 
 import java.net.InetAddress;
