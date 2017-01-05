08ca40f1ad7244a372330b9e003db209dede128f
201601180154 <zmhust88@gmail.com>
2017-01-05 16:04:40 +0800

    push middle fix bug

--- push-io/src/main/java/push/SecurePushClientHandler.java
+++ push-io/src/main/java/push/SecurePushClientHandler.java
@@ -2,13 +2,15 @@ package push;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import push.message.Entity;
 
 /**
  * Handles a client-side channel.
  */
 public class SecurePushClientHandler extends SimpleChannelInboundHandler<Entity.BaseEntity> {
-
+    private static Logger logger= LoggerFactory.getLogger(SecurePushClientHandler.class);
     @Override
     public void channelRead0(ChannelHandlerContext ctx, Entity.BaseEntity msg) throws Exception {
         if(msg.getType()==Entity.Type.MESSAGE){
@@ -19,7 +21,7 @@ public class SecurePushClientHandler extends SimpleChannelInboundHandler<Entity.
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        cause.printStackTrace();
+        logger.error("remote socket exception caught",cause);
         ctx.close();
     }
 }
